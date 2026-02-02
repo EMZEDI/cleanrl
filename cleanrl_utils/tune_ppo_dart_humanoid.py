b6d2121607ee092ab1d533f6ce25762d500bf786
Shahrad <shahrad_m@icloud.com>
2026-02-02 14:37:49 -0500

    torch compile bug fix

--- cleanrl_utils/tune_ppo_dart_humanoid.py
+++ cleanrl_utils/tune_ppo_dart_humanoid.py
@@ -87,11 +87,11 @@ def run_trial(
         "total-timesteps": total_timesteps,
         "num-envs": 64,
         "num-steps": 1024,
-        "track": False,  # No wandb on compute nodes
         "capture-video": False,
         "save-model": False,
         "seed": seed,
         "cuda": True,
+        "no-track": "",
     }
     
     # Merge parameters
